80d97eb02529f485362151f51846fa6a6fc4c14e
FrancoGimenezO <francogz19980427@gmail.com>
2024-05-30 23:22:59 -0400

    deleteServices

--- WebP1/urls.py
+++ WebP1/urls.py
@@ -21,7 +21,6 @@ from core import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
 
