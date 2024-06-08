6dbae906535df11f2c4c1902b0aa664bfb77475b
FrancoGimenezO <francogz19980427@gmail.com>
2024-06-07 20:58:35 -0400

    carga de CV

--- WebP1/settings.py
+++ WebP1/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_DIRS = (os.path.join(BASE_DIR, 'core/static'))
+STATIC_DIRS = (os.path.join(BASE_DIR, 'core/static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
